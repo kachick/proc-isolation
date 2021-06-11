fd8836f51e539490f0a7769887980d71e655d6df
Kenichi Kamiya <kachick1@gmail.com>
2021-06-11 09:29:47 +0900

    WIP

--- ext/proc/isolation.c
+++ ext/proc/isolation.c
@@ -1,54 +1,78 @@
-// #include "ruby.h"
-// #include "ruby.h"
-#include "ruby/ruby.h"
-// #include "ruby/debug.h"
-// #include "ruby/vm_core.h"
-// #include "ruby/st.h"
-// #include "vm_core.h"
+#include "ruby.h"
 
-// #define CoreDataFromValue(obj, type) (type*)DATA_PTR(obj
+#if 1
+#define CoreDataFromValue(obj, type) (type*)DATA_PTR(obj)
+#else
+#define CoreDataFromValue(obj, type) (type*)rb_data_object_get(obj)
+#endif
+#define GetCoreDataFromValue(obj, type, ptr) ((ptr) = CoreDataFromValue((obj), type))
 
-// #define GetCoreDataFromValue(obj, type, ptr) ((ptr) = CoreDataFromValue((obj), type))
 
-// #define GetProcPtr(obj, ptr) GetCoreDataFromValue((obj), rb_proc_t, (ptr))
+#define GetProcPtr(obj, ptr) \
+  GetCoreDataFromValue((obj), rb_proc_t, (ptr))
+
+#ifndef rb_iseq_t
+typedef struct rb_iseq_struct rb_iseq_t;
+#define rb_iseq_t rb_iseq_t
+#endif
+struct rb_captured_block {
+    VALUE self;
+    const VALUE *ep;
+    union {
+	const rb_iseq_t *iseq;
+	const struct vm_ifunc *ifunc;
+	VALUE val;
+    } code;
+};
+
+enum rb_block_handler_type {
+    block_handler_type_iseq,
+    block_handler_type_ifunc,
+    block_handler_type_symbol,
+    block_handler_type_proc
+};
+
+enum rb_block_type {
+    block_type_iseq,
+    block_type_ifunc,
+    block_type_symbol,
+    block_type_proc
+};
+
+struct rb_block {
+  union {
+	struct rb_captured_block captured;
+	VALUE symbol;
+	VALUE proc;
+  } as;
+  enum rb_block_type type;
+};
 
 typedef struct {
-    // const struct rb_block VALUE;
+    const struct rb_block block;
     unsigned int is_from_method: 1;	/* bool */
     unsigned int is_lambda: 1;		/* bool */
     unsigned int is_isolated: 1;        /* bool */
 } rb_proc_t;
 
 
-// VALUE
-// rb_proc_lambda_p(VALUE procval)
-// {
-//     rb_proc_t *proc;
-//     GetProcPtr(procval, proc);
-
-//     return proc->is_lambda ? Qtrue : Qfalse;
-// }
-
 VALUE rb_proc_isolate(VALUE);
 VALUE rb_proc_isolate_bang(VALUE);
-// VALUE rb_proc_isolated_p(VALUE);
+VALUE rb_proc_isolated_p(VALUE);
 
-// VALUE
-// rb_proc_isolated_p(VALUE procval)
-// {
-//     rb_proc_t *proc;
-//     GetProcPtr(procval, proc);
+VALUE
+rb_proc_isolated_p(VALUE procval)
+{
+    rb_proc_t *proc;
+    GetProcPtr(procval, proc);
 
-//     return proc->is_isolated ? Qtrue : Qfalse;
-// }
-
-
-        // proc->is_isolated = TRUE;
+    return proc->is_isolated ? Qtrue : Qfalse;
+}
 
 void
 Init_isolation(void)
 {
   rb_define_method(rb_cProc, "isolate", rb_proc_isolate, 0);
   rb_define_method(rb_cProc, "isolate!", rb_proc_isolate_bang, 0);
-  // rb_define_method(rb_cProc, "isolated?", rb_proc_isolated_p, 0);
+  rb_define_method(rb_cProc, "isolated?", rb_proc_isolated_p, 0);
 }
--- test/proc/test_isolation.rb
+++ test/proc/test_isolation.rb
@@ -20,7 +20,9 @@ class TestProcIsolation < Test::Unit::TestCase
 
   def test_proc_isolate!
     prc = proc { 42 }
+    assert_false(prc.isolated?)
     assert_instance_of(Proc, prc.isolate!)
+    assert_true(prc.isolated?)
     assert_same(prc, prc.isolate!)
     assert do
       !prc.isolate!.lambda?
@@ -46,7 +48,9 @@ class TestProcIsolation < Test::Unit::TestCase
 
   def test_lambda_isolate!
     lmd = -> { 42 }
+    assert_false(lmd.isolated?)
     assert_instance_of(Proc, lmd.isolate!)
+    assert_true(lmd.isolated?)
     assert_same(lmd, lmd.isolate!)
     assert do
       lmd.isolate!.lambda?
@@ -60,6 +64,7 @@ class TestProcIsolation < Test::Unit::TestCase
       lmd.isolate!
     end
     assert_equal(42, lmd.call)
+    assert_false(lmd.isolated?)
   end
 
   def test_proc_isolate
@@ -70,6 +75,7 @@ class TestProcIsolation < Test::Unit::TestCase
       !prc.isolate.lambda?
     end
     assert_equal(42, prc.isolate.call)
+    assert_true(prc.isolate.isolated?)
 
     foo = 42
     prc = proc { foo }
@@ -90,6 +96,7 @@ class TestProcIsolation < Test::Unit::TestCase
   def test_lambda_isolate
     lmd = -> { 42 }
     assert_instance_of(Proc, lmd.isolate)
+    assert_true(lmd.isolate.isolated?)
     assert_not_same(lmd, lmd.isolate)
     omit_if(!lmd.isolate.lambda?) do
       assert do
@@ -97,12 +104,21 @@ class TestProcIsolation < Test::Unit::TestCase
       end
     end
     assert_equal(42, lmd.isolate.call)
+    assert_false(lmd.isolated?)
 
     foo = 42
     lmd = -> { foo }
 
     assert_raise_with_message(ArgumentError, /can not isolate a Proc because it accesses outer variables/) do
       lmd.isolate
+    end
+  end
+
+  def test_proc_isolated_p_as_an_unbound_method
+    unbound = Proc.instance_method(:isolated?)
+
+    assert_raise_with_message(TypeError, /bind argument must be an instance of Proc/) do
+      unbound.bind(Object.new)
     end
   end
 end
